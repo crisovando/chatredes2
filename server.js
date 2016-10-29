5300c67ecc81dc20f591b06a45e6b04f600452ff
Cristian <fabian222_90@hotmail.com>
2016-10-29 08:36:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(http);
 
 
 var port = process.env.PORT || 8080;
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || port
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.set('views', __dirname + '/views');
